0ccca59f4151253ca0f19d3baf790cbb307cc454
Sai Teja Nagamothu <saitejajnvcc@gmail.com>
2017-04-20 18:25:03 +0530

    Add files via upload

---
+++ LRUCache.java
@@ -0,0 +1,39 @@
+package ExtraWork;
+
+
+import java.util.LinkedList;
+
+
+/**
+ *
+ * @author saiteja21
+ */
+public class LRUCache {
+    
+    static public int lruFunction(int max_cache_size,int frames[],int frame_size){
+         LinkedList<Integer> current_cache = new LinkedList<>();
+         int i,temp,index,count=0;
+         if(frame_size>0){
+             for(i=0;i<max_cache_size;i++){
+                count++;
+                current_cache.add(frames[i]);
+             }
+         }else{
+             return 0;
+         }
+         for(; i < frames.length; i++) {
+            if(current_cache.contains(frames[i])){
+               temp = frames[i];
+               index = current_cache.indexOf(temp);
+               current_cache.remove(index);
+               current_cache.addLast(temp);
+            }else{
+                count++;
+                temp = frames[i];
+                current_cache.removeFirst();
+                current_cache.addLast(temp);
+             } 
+         }
+       return count; 
+    }    
+}
